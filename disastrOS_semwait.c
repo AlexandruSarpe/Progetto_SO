4f49548cab5078a10491273156c2104d02c02966
jack19952 <sarpe.1692247@studenti.uniroma1.it>
2018-03-07 20:25:20 +0100

    fix semwait

--- disastrOS_semwait.c
+++ disastrOS_semwait.c
@@ -8,13 +8,13 @@
 
 void internal_semWait(){
   // do stuff :)
-  int semnum=running->syscall_args[0];
+  int fd=running->syscall_args[0];
 
 
   ListHead process_semaphores = running->sem_descriptors;
 
   //we have to check if the semaphore is opened in the process
-  Semaphore* sem = Search_id(&process_semaphores, semnum);
+  Semaphore* sem = SemDescriptorList_byFd(&process_semaphores, fd);
 
   if(!sem){
     running->syscall_retvalue = DSOS_ESEMNOTOPENED;
@@ -22,15 +22,22 @@ void internal_semWait(){
 
   else if(sem->count<=0){
     //the process has to wait in the waiting queue
-    List_insert(&sem->waiting_descriptors, sem->waiting_descriptors.last, (ListItem*)&running);
+    List_insert(&sem->waiting_descriptors, sem->waiting_descriptors.last, (ListItem*)running);
     //we have to change the state of the process_semaphores
     running->status = Waiting;
     //and we insert the process in the waiting list
-    List_insert(&waiting_list, waiting_list.last, (ListItem*)&running);
-    running->syscall_retvalue = 0;
+    List_insert(&waiting_list, waiting_list.last, (ListItem*)running);
+    //if there is a process in wating list of the semaphore we have to awake him, else we have to set speeper as run prcess
+    if (ready_list.first)
+      running=(PCB*) List_detach(&ready_list, ready_list.first);
+    else {
+      running=0;
+      printf ("they are all sleeping\n");
+      running->syscall_retvalue = 0;
+    }
   }
   else{
-    sem->count --;
+    (sem->count)--;
     running->syscall_retvalue = 0;
   }
 }
