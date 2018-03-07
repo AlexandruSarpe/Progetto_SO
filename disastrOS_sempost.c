b9ab1b871fd9f1b57a85f1a92267f667357ade3c
cnunzio31 <c.nunzio31.nc@gmail.com>
2018-03-07 08:49:46 +0100

    added shift from 'waiting queue' to 'ready queue'

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -7,17 +7,37 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semPost(){
-  // do stuff :)
-  // Prendo argomento numero del semaforo
-  int semNumber=running->syscall_args[0];
+    // do stuff :)
+    // Prendo argomento numero del semaforo
+    int semNumber=running->syscall_args[0];
 
-  // Prendo la lista dei semafori attivi per quel processo
-  ListHead sems=running->sem_descriptors;
+    // Prendo la lista dei semafori attivi per quel processo
+    ListHead sems=running->sem_descriptors;
 
-  // Controllo se il semaforo esiste ed è nella lista
-  Semaphore* semaphore=Search_id(&sems, semNumber);
+    // Controllo se il semaforo esiste ed è nella lista
+    SemDescriptor* sem_d=Search_id(&sems, semNumber);
 
-  // se non c'è;
-  if (!sem) running->syscall_retvalue=DSOS_ESEMNOTOPENED;
+    // se non c'è;
+    if (!sem_d) running->syscall_retvalue=DSOS_ESEMNOTOPENED;
 
+    // prendo il valore attuale del semaforo e lo aumento di 1
+    int count=(sem_d->semaphore->count)++;
+
+    // prendo la lista dei processi in waiting che usano quel semaforo
+    ListHead w_desc=sem_d->semaphore->waiting_descriptors;
+
+    if (count>0) {
+        // rimuovo il processo dalla waiting queue
+        List_detach(&waiting_list,(ListItem*)running);
+
+        // inserisco il processo nella ready queue
+        List_insert(&ready_list,ready_list.last,(ListItem*)running);
+
+        // rimuovo il processo dai waiting descriptors del semaforo
+        List_detach(&w_desc,(ListItem*)running);
+
+        //TODO
+    }
+
+    return 0;
 }
